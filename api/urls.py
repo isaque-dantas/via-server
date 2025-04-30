df27f366462651e81965868cea7109ef3faa4270
isaque-dantas <vicisaque413@gmail.com>
2025-04-30 16:34:18 -0300

    feat: setup routes

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,19 @@
 from django.urls import path
 
-from api.views.user import UserViewSet
+from api.views.customer import CustomerViewSet, SingleCustomerViewSet
+from api.views.order import OrderViewSet, SingleOrderViewSet
+from api.views.product import ProductViewSet, SingleProductViewSet
+from api.views.employee import EmployeeViewSet
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
-    path('user', UserViewSet.as_view()),
+    path('employee', EmployeeViewSet.as_view()),
+    path('product', ProductViewSet.as_view()),
+    path('product/<int:pk>', SingleProductViewSet.as_view()),
+    path('customer', CustomerViewSet.as_view()),
+    path('customer/<int:pk>', SingleCustomerViewSet.as_view()),
+    path('order', OrderViewSet.as_view()),
+    path('order/<int:pk>', SingleOrderViewSet.as_view()),
     path('token', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh', TokenRefreshView.as_view(), name='token_refresh'),
 ]
