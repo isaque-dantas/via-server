749e097697b7ae67eb455b19162e947f22727bad
isaque-dantas <vicisaque413@gmail.com>
2025-05-03 15:46:36 -0300

    feat: add mock data

--- api/tests/utils.py
+++ api/tests/utils.py
@@ -3,6 +3,10 @@ from rest_framework.test import APIClient
 from api import BASE_URL
 from api.models import Employee
 from api.models.customer import Customer
+from api.models.order import Order
+from api.models.product import Product
+from api.serializers.order import OrderSerializer
+from api.services.order import OrderService
 
 
 class Utils:
@@ -14,7 +18,7 @@ class Utils:
 
     customer_data = {
         'email': 'cliente@email.com',
-        'name': 'Usuário Cliente'
+        'name': 'Cliente Genérico'
     }
 
     product_data = {
@@ -46,3 +50,84 @@ class Utils:
         customer = Customer.objects.create(**cls.customer_data)
 
         return employee, customer
+
+    @classmethod
+    def add_example_data(cls):
+        if Employee.objects.filter(email=cls.employee_data['email']).exists():
+            e = Employee.objects.get(email=cls.employee_data['email'])
+        else:
+            e = Employee.objects.create_user(**cls.employee_data)
+
+        # --- --- --- PRODUTOS --- --- --- #
+
+        Product.objects.all().delete()
+
+        cosmetico = Product.objects.create(**cls.product_data)
+
+        abacates = Product.objects.create(
+            name='Abacates do Francisco',
+            description='Abacates fresquinhos da horta do Francisco, sem agrotóxicos!',
+            price=20.75,
+        )
+
+        sapv = Product.objects.create(
+            name='Motor de cálculo do SAPV',
+            description='API em F# com mais de 200 testes automatizados.',
+            price=100000,
+        )
+
+        coca_cola = Product.objects.create(
+            name='Coca-Cola',
+            description='Você já sabe.',
+            price=4.50,
+        )
+
+        # --- --- --- CLIENTES --- --- --- #
+
+        Customer.objects.all().delete()
+
+        eletrobras = Customer.objects.create(
+            email='eletrobras@email.com',
+            name='Eletrobras'
+        )
+
+        logap = Customer.objects.create(
+            email='logap@email.com',
+            name='LogAp Sistemas'
+        )
+
+        # --- --- --- PEDIDOS --- --- --- #
+        Order.objects.all().delete()
+
+        serializer = OrderSerializer(data={
+            'employee': e,
+            'customer': logap.id,
+            'date': '2020-01-01',
+            'description': 'Almoço dos funcionários.',
+            'products': [{'id': abacates.id, 'quantity': 1500}, {'id': coca_cola.id, 'quantity': 750}]
+        })
+        serializer.is_valid()
+        print(serializer.errors)
+        OrderService.create(serializer.validated_data)
+
+        serializer = OrderSerializer(data={
+            'employee': e,
+            'customer': eletrobras.id,
+            'date': '2020-01-01',
+            'products': [{'id': sapv.id, 'quantity': 1}]
+        })
+        serializer.is_valid()
+        OrderService.create(serializer.validated_data)
+
+        serializer = OrderSerializer(data={
+            'employee': e,
+            'customer': eletrobras.id,
+            'date': '2020-01-01',
+            'products': [
+                {'id': cosmetico.id, 'quantity': 34},
+                {'id': abacates.id, 'quantity': 3000},
+                {'id': coca_cola.id, 'quantity': 4052}
+            ]
+        })
+        serializer.is_valid()
+        OrderService.create(serializer.validated_data)
